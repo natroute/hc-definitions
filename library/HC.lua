3e5c6f26e9abf0b41704a823f4cf701f8d10cae0
natroute <247222395+natroute@users.noreply.github.com>
2026-09-07 17:22:08 +0300

    remove leftover

--- library/HC.lua
+++ library/HC.lua
@@ -223,7 +223,6 @@ function Collider:hash() end
 ---
 ---Most of the time, HC will be run as a singleton; you can, however, also create several instances using [`HC.new()`](lua://HC.new) or `HC()`, which will each hold their own little worlds.
 ---
----@class HC
 local module = {}
 
 ---
